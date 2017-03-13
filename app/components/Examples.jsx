fc60db065cea6e26509aec40c77d2fb892b2cddf
Dustin K Greco <dgreco.dev@gmail.com>
2017-03-13 12:05:21 -0600

    Styles added to Main and Examples jsx pages

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,11 +1,23 @@
 const React = require('react');
+let {Link} = require('react-router');
 
 let Examples = () => {
   "use strict";
   return (
       <div>
-          <h3>Examples</h3>
-          <p>Welcome to the Examples Page Hunter Jung!</p>
+          <h1 className="text-center">Examples</h1>
+          <p>Here are a few examples to try:</p>
+          <ol>
+              <li>
+                  <Link to="/?city=Broomfield">Broomfield, CO</Link>
+              </li>
+              <li>
+                  <Link to="/?city=Pheonix">Pheonix, AZ</Link>
+              </li>
+              <li>
+                  <Link to="/?city=Mission%20Viejo">Mission Viejo, CA</Link>
+              </li>
+          </ol>
       </div>
   );
 };
